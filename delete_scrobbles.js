ac6c75ed3286c289088142cad51c4df538b852ba
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2020-10-06 21:16:45 -0400

    Create delete_scrobbles.js

---
+++ delete_scrobbles.js
@@ -0,0 +1,48 @@
+// Replace DELETE_MATCHING with the text on the scrobbles you want to delete
+const DELETE_MATCHING = "Your text here";
+
+const ROW_CLS = ".chartlist-row";
+const DELETE_CLS = ".more-item--delete";
+const SLEEP_FOR = 800;
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function get_rows() {
+  const rows_nl = document.querySelectorAll(ROW_CLS);
+  const rows = Array.from(rows_nl);
+
+  return rows;
+}
+
+
+function handle_row(row, match) {
+  match = match.toLowerCase();
+  const text = row.textContent.toLowerCase();
+
+  if (text.includes(artist)) {
+    const delete_btn = row.querySelector(DELETE_CLS);
+    delete_btn.click();
+  }
+}
+
+
+async function delete_tracks(rows, match) {
+  for (const row of rows) {
+    handle_row(row, match);
+
+    await sleep(SLEEP_FOR);
+  }
+}
+
+
+async function main() {
+  const rows = get_rows();
+  await delete_tracks(rows, DELETE_MATCHING);
+}
+
+
+await main()
