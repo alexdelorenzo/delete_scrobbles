02509d2fede6d5ac3f982e074cbf89019dba6bc6
alexdelorenzo <you@example.com>
2020-12-26 22:16:45 -0500

    Tabs -> spaces

--- delete_scrobbles.js
+++ delete_scrobbles.js
@@ -67,12 +67,12 @@ async function main() {
     const rows = get_rows();
     await delete_tracks(rows, DELETE_MATCHING);
 
-  	if (ALL_PAGES && next_page()) {
-  	  await sleep(PAGE_SLEEP);
-  	  continue;
-  	} else {
-  	  break;
-  	}
+    if (ALL_PAGES && next_page()) {
+      await sleep(PAGE_SLEEP);
+       continue;
+     } else {
+       break;
+     }
   }
 }
 
